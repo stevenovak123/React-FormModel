1fd79af44257ed9191e7a33230e8c95dc8bce41c
stevenovak123 <stevenovak0802@gmail.com>
2021-05-19 13:14:20 +0530

    added basic form

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Card from '../UI/Card'
+import styles from './AddUser.module.css'
+import Button from '../UI/Button'
+function AddUser(props) {
+    const addUserHandler=(event)=>{
+        event.preventDefault();
+
+    }
+    return (
+        <Card className={styles.input}>
+
+        <form onSubmit={addUserHandler}>
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text"></input>
+            <label htmlFor="age">Age(inYears)</label>
+            <input id="age" type="number"></input>
+            <Button type="submit">Add User</Button>
+         </form>           
+        </Card>
+            )
+}
+
+export default AddUser
